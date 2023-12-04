7826807d406937482d7de4b9fa260a6ee9b9131f
Walie <whoumad@gmail.com>
2023-12-04 01:38:24 +0100

    font awesome optimisé

--- src/main.js
+++ src/main.js
@@ -5,15 +5,9 @@ import App from './App.vue'
 /* import the fontawesome core */
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
-import { fas } from "@fortawesome/free-solid-svg-icons";
 import { faTwitter, faFacebook, faStackOverflow, faGithub, faLinkedin, faInstagram } from '@fortawesome/free-brands-svg-icons'
 
-library.add(fas);
 library.add(faTwitter, faFacebook, faStackOverflow, faGithub, faLinkedin, faInstagram);
-
-/* add icons to the library */
-library.add(faUserSecret)
 
 createApp(App)
     .component('font-awesome-icon', FontAwesomeIcon)
